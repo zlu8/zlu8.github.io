09f2db3957f30323e756f23a8c6a50c498774477
zlu8 <Brian_0410@hotmail.com>
2015-10-29 20:01:58 -0400

    Add my website

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function whenPageReady() {
+    //do the coding
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+}
+
+$(document).ready(whenPageReady)
